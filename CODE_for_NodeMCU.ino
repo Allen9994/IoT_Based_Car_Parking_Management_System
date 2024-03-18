0d2c051179a53723212c17c905614be813fcdde4
Allen John <88543723+Allen9994@users.noreply.github.com>
2024-03-19 02:14:33 +0530

    Update CODE_for_NodeMCU.ino
    Simplified Further

--- CODE_for_NodeMCU.ino
+++ CODE_for_NodeMCU.ino
@@ -4,8 +4,6 @@
 #define BLYNK_TEMPLATE_ID "TMPLZXC-QBoy"
 #define BLYNK_DEVICE_NAME "DEVICE NAME OF THE BLYNK DEVICE"
 #define BLYNK_AUTH_TOKEN "BLYNK AUTH TOKEN"
-
-
 #define BLYNK_PRINT Serial
 
 #include <SPI.h>
@@ -23,7 +21,6 @@
 
 char auth[] = BLYNK_AUTH_TOKEN;
 
-
 //************************************************************************
 
 #define SS_PIN  D2  
@@ -32,22 +29,21 @@ char auth[] = BLYNK_AUTH_TOKEN;
 MFRC522 mfrc522(SS_PIN, RST_PIN); // Create MFRC522 instance.
 //************************************************************************
 /* Set these to your desired credentials. */
-const char *ssid = "Wi-Fi SSID";
-const char *password = "Wi-Fi PASSWORD";
-const char* device_token  = "TOKEN ID OF THE WEB-APP";
+const char *ssid =         "Wi-Fi SSID";
+const char *password =     "Wi-Fi PASSWORD";
+const char* device_token = "TOKEN ID OF THE WEB-APP";
 
 BlynkTimer timer;
 //************************************************************************
 
-
 X509List cert(TELEGRAM_CERTIFICATE_ROOT);
 WiFiClientSecure secured_client;
 //************************************************************************
-String URL = "http://IP-ADDRESS/rfidattendance/getdata.php"; //computer IP or the server domain
+String URL = "http://IP-ADDRESS/rfidattendance/getdata.php"; //computer IP or the server domain.
 String getData, Link, OldCardID = "";
 unsigned long previousMillis = 0;
-int n,p1,p2,p3,p4,p5,p6,le=1;
-String msg; //To extract serial message from Arduino
+int n, p1, p2, p3, p4, p5, p6, nearest_slot = 1;
+String slot_information; //To extract serial message from Arduino
 //************************************************************************
 
 void setup() {
@@ -88,7 +84,7 @@ void loop() {
   if ( ! mfrc522.PICC_ReadCardSerial()) {
     return;//if read card serial(0) returns 1, the uid struct contians the ID of the read card.
   }
-  String CardID ="";
+  String CardID = "";
   for (byte i = 0; i < mfrc522.uid.size; i++) {
     CardID += mfrc522.uid.uidByte[i];
   }
@@ -105,53 +101,52 @@ void loop() {
   delay(1000);
 }
 
-
-void message(String y, String token, String chat)
+void nearestParkingSlot()
 {
-  UniversalTelegramBot bot(token, secured_client);  
-  //Starts Telegram bot using the Token of each user's telegram.
-  bot.sendMessage(chat, y, ""); 
-  //Sends the message obtained from the calling function 'y'. It uses chat ID to identify the user.
+         if(p1 == 0) nearest_slot = 1;
+    else if(p2 == 0) nearest_slot = 2;
+    else if(p3 == 0) nearest_slot = 3;
+    else if(p4 == 0) nearest_slot = 4;
+    else if(p5 == 0) nearest_slot = 5;
+    else if(p6 == 0) nearest_slot = 6;
 }
 
-void telegram(String CI, String un, int amount, int time_, String adm, String token, String chat)
+void sendMessage(String message, String token, String chat)
 {
-    if(adm == "login")  //If the user is entering the lot.
+  delay(100); //Starts Telegram bot using the Token of each user's telegram.
+  Serial.println(message);
+  UniversalTelegramBot bot(token, secured_client);  
+  //Sends the message obtained from the calling function 'y'. It uses chat ID to identify the user.
+  bot.sendMessage(chat, message, ""); 
+}
+
+void telegram(String CI, String username, int amount, int time_, String admission, String token, String chat)
+{
+    if(admission == "login")  //If the user is entering the lot.
     {
-      String y;
-      if(le == 1 || le == 2)
-      {
-        y = "turn left and park your car";
-      }else if(le == 3 || le == 4)
-      {
-        y = "head straight, turn left and park your car";
-      }
-      else if(le == 5 || le == 6)
-      {
-        y = "turn right and park your car along the wall";
-      }
-      String n = "Welcome " + un + ". Your account balance is : " + String(amount) + ". Your nearest slot is " 
-      + String(le) + ". Kindly " + String(y); //Produces the welcome message to be sent to their telegram.
-      Serial.println(n);
-      delay(100);
-      message(n,token,chat); //Calls the sending function.
+      String directions;
+           if(nearest_slot == 1 || nearest_slot == 2)    directions = "turn left and park your car";
+      else if(nearest_slot == 3 || nearest_slot == 4)    directions = "head straight, turn left and park your car";
+      else if(nearest_slot == 5 || nearest_slot == 6)    directions = "turn right and park your car along the wall";
+      
+      String message = "Welcome " + username + ". Your account balance is : " + String(amount) + ". Your nearest slot is " 
+      + String(nearest_slot) + ". Kindly " + String(directions); //Produces the welcome message to be sent to their telegram.
+      sendMessage(message, token, chat); //Calls the sending function.
     }
-    if(adm == "logout") //If the user is entering the lot.
+    if(admission == "logout") //If the user is entering the lot.
     {//Produces the exit message to be sent to their telegram.
-      String n;
+      String message;
       if(amount >= 0)
       {
-        n = "Dear " + un + ". You have spent " + String(time_) 
+        message = "Dear " + username + ". You have spent " + String(time_) 
         + " seconds inside the parking lot. Your remaining balance is : " + String(amount); 
       }else
       {
-        n = "Dear " + un + ". You have spent " + String(time_) 
+        message = "Dear " + username + ". You have spent " + String(time_) 
         + " seconds inside the parking lot. You have a pending debt of " + String(-amount) 
         + ". Kindly clear your dues before your next arrival."; 
       } 
-      Serial.println(n);
-      delay(100);
-      message(n,token,chat);//Calls the sending function.
+      sendMessage(message, token, chat);//Calls the sending function.
     }  
 }
 
@@ -187,7 +182,6 @@ void SendCardID( String Card_uid ){
        delay(1000);
        digitalWrite(D4,LOW);
     }else
-
     if (httpCode == 200) {  //httpCode = 200 => connection is successful and NodeMCU can receive desired data from the MyPHPAdmin.
                             //httpCode =  -1 => error in connection.
      if (payload == "succesful") {  //During edit stage, if card is new and read succesfully.
@@ -202,20 +196,20 @@ void SendCardID( String Card_uid ){
         int y = 0;  //Initialising y = 0. y is used to measure size of each line.
         while(payload[y] != '{'){  //When payload reaches '{' symbol, measures the number of decimal places in amount.
           y++;
-        }int z= y;
+        }int z = y;
         while(payload[z] != '/'){  //When payload reaches '/' symbol, measures the number of decimal places in time remaining.
           z++;
-        }int a= z;        
+        }int a = z;        
         while(payload[a] != '}'){ //When payload reaches '}' symbol, measures the number of decimal places in telegram token.
           a++;
-        }int b= a;        
+        }int b = a;        
         while(payload[b] != 'l'){  //When payload reaches 'l' symbol, measures the number of decimal places in telegram chat ID.
           b++;
         }
-        int j = payload.substring(0,y).toInt(); //Extracts and converts the amount to integer variable j.
+        int amount = payload.substring(0,y).toInt(); //Extracts and converts the amount to integer variable j.
         int ti = payload.substring(y+1,z).toInt();  //Extracts and converts the time remaining to integer variable ti.
-        String token = payload.substring(z+1,a);  //Extracts  the token to String variable token.
-        String chat = payload.substring(a+1,b);   //Extracts  the tchat ID to String variable chat.
+        String token = payload.substring(z+1,a);  //Extracts the token to String variable token.
+        String chat = payload.substring(a+1,b);   //Extracts the chat ID to String variable chat.
         if (payload.substring(b, b+5) == "login") { //When there's enough balance and valid card, allows user to enter.
          digitalWrite(D3,HIGH); digitalWrite(D4,HIGH); //Sending signal 11 to Arduino         
          delay(1000);
@@ -223,16 +217,16 @@ void SendCardID( String Card_uid ){
            
          String user_name = payload.substring(b+5); //Extracting the name of user from payload and storing in user_name.
          Serial.println(user_name);
-         telegram(Card_uid,user_name,j,ti,"login",token,chat);  //Calling telegram function  
+         telegram(Card_uid, user_name, amount, ti, "login", token, chat);  //Calling telegram function  
           // and sending all the extracted information to telegram function
         }
         else if (payload.substring(b, b+6) == "logout") { //When the user is exiting.
-          digitalWrite(D3,HIGH); digitalWrite(D4,HIGH); //Sending signal 11 to Arduino        
+          digitalWrite(D3,HIGH);  digitalWrite(D4,HIGH); //Sending signal 11 to Arduino        
           delay(1000);
-          digitalWrite(D3,LOW);digitalWrite(D4,LOW);         
+          digitalWrite(D3,LOW);   digitalWrite(D4,LOW);         
           String user_name = payload.substring(b+6);  //Extracting the name of user from payload and storing in user_name.
           Serial.println(user_name);
-          telegram(Card_uid,user_name,j,ti,"logout",token,chat);  //Calling telegram function 
+          telegram(Card_uid, user_name, amount, ti, "logout", token, chat);  //Calling telegram function 
       }  //and sending all the extracted information to telegram function.    
     }
       delay(100);
@@ -243,11 +237,11 @@ void SendCardID( String Card_uid ){
 
 void Blynk_function()
 {
-      msg = Serial.readStringUntil('\r'); //extracts the entire string
-        if(msg!="\n") //To read till entire line
+        slot_information = Serial.readStringUntil('\r'); //extracts the entire string
+        if(slot_information != "\n") //To read till entire line
         {                     //Here, the data from arduino is converted from string to integer. 
                               //Then it each of its digit is stored in individual integer variables 
-            n = msg.toInt();
+            n = slot_information.toInt();
             p1 = n/100000;    //For eg. msg = 110010. p1 = 1 p2 = 1 p3 = 0 p4 = 0 p5 = 1 p6 = 0 
             n = n - p1*100000;
             p2 = n/10000;
@@ -259,30 +253,22 @@ void Blynk_function()
             p5 = n/10;
             n = n - p5*10;
             p6 = n;
-            String ss;
-            ss = String(le)+String(p1)+String(p2)+String(p3)+String(p4)+String(p5)+String(p6);
-            if(p1 == 0)      le = 1;
-            else if(p2 == 0) le = 2;
-            else if(p3 == 0) le = 3;
-            else if(p4 == 0) le = 4;
-            else if(p5 == 0) le = 5;
-            else if(p6 == 0) le = 6;
-            Serial.println(ss);
-            Blynk.virtualWrite(V7, 6-p1-p2-p3-p4-p5-p6); 
+            nearestParkingSlot();
+            Blynk.virtualWrite(V7, 6 - p1 - p2 - p3 - p4 - p5 - p6); 
             //Sends remaining slot information to blynk's 7th virtual pin.
             //Here it turns on/off the Blynk LED depending on the value of p which was extracted before.
-            if(p1==1) Blynk.virtualWrite(V1, HIGH);
-            else      Blynk.virtualWrite(V1, LOW);
-            if(p2==1) Blynk.virtualWrite(V2, HIGH);
-            else      Blynk.virtualWrite(V2, LOW);
-            if(p3==1) Blynk.virtualWrite(V3, HIGH);
-            else      Blynk.virtualWrite(V3, LOW);
-            if(p4==1) Blynk.virtualWrite(V4, HIGH);
-            else      Blynk.virtualWrite(V4, LOW);
-            if(p5==1) Blynk.virtualWrite(V5, HIGH);
-            else      Blynk.virtualWrite(V5, LOW);
-            if(p6==1) Blynk.virtualWrite(V6, HIGH);
-            else      Blynk.virtualWrite(V6, LOW);
+            if(p1 == 1)  Blynk.virtualWrite(V1, HIGH);
+            else         Blynk.virtualWrite(V1, LOW);
+            if(p2 == 1)  Blynk.virtualWrite(V2, HIGH);
+            else         Blynk.virtualWrite(V2, LOW);
+            if(p3 == 1)  Blynk.virtualWrite(V3, HIGH);
+            else         Blynk.virtualWrite(V3, LOW);
+            if(p4 == 1)  Blynk.virtualWrite(V4, HIGH);
+            else         Blynk.virtualWrite(V4, LOW);
+            if(p5 == 1)  Blynk.virtualWrite(V5, HIGH);
+            else         Blynk.virtualWrite(V5, LOW);
+            if(p6 == 1)  Blynk.virtualWrite(V6, HIGH);
+            else         Blynk.virtualWrite(V6, LOW);
        }
 }
 //********************connect to the WiFi******************
